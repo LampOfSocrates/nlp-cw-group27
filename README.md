af6ebfa229a4a4bbbfb110774596c0a7405a5fdf
Lamp Socrates <lampofsocrates@gmail.com>
2024-05-18 20:26:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # nlp-cw-group27
+
+This is the code repository for the group work for NLP Coursework ....
+
+This is a flask app that downloads Huffing Face Models and uses them to do NER tasks
+
+## Installation 
+
+`git clone git@github.com:LampOfSocrates/nlp-cw-group27.git`
+`cd nlp-cw-group27`
+`pip install -r requirements.txt`
+
+
+## Access to Hugging Face
+
+If the code doesnt work , you need access to HuggingFace
+
+
+## Running the app 
+
+`python app_flask_plodcw.py`
+
+this should launch a server on http://127.0.0.1:5000/
---
+++ app_flask_plodcw.py
@@ -0,0 +1,45 @@
+from spacy.tokens import Doc, Span
+from spacy import displacy
+import spacy
+from flask import Flask, jsonify, render_template, request, make_response
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+# Make a Flask App
+app = Flask(__name__)
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+tokenizer = AutoTokenizer.from_pretrained("LampOfSocrates/bert-base-cased-sourav")
+model = AutoModelForTokenClassification.from_pretrained("LampOfSocrates/bert-base-cased-sourav")
+
+# Load the NER model and tokenizer from Hugging Face
+#ner_pipeline = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english")
+ner_pipeline = pipeline("ner", model=model, tokenizer = tokenizer)
+nlp = spacy.load("en_core_web_sm") # only for rendering
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        text = request.form['message']
+        entities = ner_pipeline(text)
+    
+        # Convert HF entities to SpaCy's format
+        doc = nlp(text)
+        ents = []
+        for entity in entities:
+            start = entity['start']
+            end = entity['end']
+            label = entity['entity']
+            span = doc.char_span(start, end, label=label)
+            if span is not None:
+                ents.append(span)
+        doc.ents = ents
+
+        # Render using displacy
+        html = displacy.render(doc, style='ent', page=False)
+        return render_template('index.html', original_text=text, styled_text=html, entities=entities)
+    else:
+        return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
